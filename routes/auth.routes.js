78bc9c4e157b24f08f2d4c01ab20c71fe80501f2
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2021-03-11 23:44:18 +0300

    fix: cors

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,18 +8,15 @@ const loader = multer({
 });
 module.exports = function (app) {
   app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Origin",
+
       " Access-Control-Allow-Methods",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-  // app.post(
-  //   "/api/auth/signup",
-  //   loader.single("avatar"),
-  // );
 
   app.post(
     "/api/auth/signup",
