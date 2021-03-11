41d7352555f5e4aacaab46d054bf7d7cba8fd6a9
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2021-03-11 23:32:19 +0300

    feat: add headers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,11 +10,12 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Origin",
+      " Access-Control-Allow-Methods",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-
   // app.post(
   //   "/api/auth/signup",
   //   loader.single("avatar"),
